e43a1dd3fd71f17fc670cf0988388af0ab8f9967
MySweetLord <118974826+MySweetLord@users.noreply.github.com>
2022-12-12 12:58:34 +0100

    Add files via upload

--- 137.py
+++ 137.py
@@ -1,9 +1,48 @@
+
+
 """
 projketuppgift 137
 """
 
 import random 
 import math as µ
+
+def area_input():
+
+    a = True
+    while a == True:
+        try:
+            print("Enter area of powerplant(m^2):", end="")
+            area = float(input())
+            if area <= 0: raise ValueError
+            a = False
+        except ValueError:
+            print("Enter positiv numerical value only")
+    return int(area)
+    
+def prop_konst_input():
+    a = True
+    while a == True:
+        try:
+            print("Enter proportionality constant of powerplant:", end="")
+            prop_konst = float(input())
+            if prop_konst <= 0: raise ValueError
+            a = False
+        except ValueError:
+            print("Enter positive numerical value only")
+    return prop_konst
+
+def latitud_input():
+    a = True
+    while a == True:
+        try:
+            print("Enter latitude of location of powerplant:", end="")
+            latitud = float(input())
+            if latitud < 0 or latitud > 90: raise ValueError
+            a = False
+        except ValueError:
+            print("Enter numerical value only")
+    return latitud
 
 class Kraftverk:
 
@@ -46,53 +85,95 @@ class Kraftverk:
 
         return std_dev
 
-    def detail_data(self, all_data_list):
+    def detail_data(self, file, all_data_list):
+        file = open(file, "w")
         n = 0
         month = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"]
     
         x = 0
         X = 1
         for i in range(12):
-            print("="*62)
-            print(month[n])
-            print("_"*100)
+            file.write("="*100+"\n")
+            file.write(month[n]+"\n")
+            file.write("_"*100+"\n")
             gap = "   "
-            print(f"{'Dag' :3s} {gap} {'Area(m2)' :10s} {gap} {'Latitud' :10s} {gap} {'Prop_konst(kWh)' :10s} {gap} {'Exponering' :10s} {gap} {'v' :10s} {gap} {'Produktion(W)' :10s}")
-            print("_"*100)
+            file.write(f"{'Dag' :3s} {gap} {'Area(m2)' :10s} {gap} {'Latitud' :10s} {gap} {'Prop_konst(kWh)' :10s} {gap} {'Exponering' :10s} {gap} {'v' :10s} {gap} {'Produktion(W)' :10s}"+"\n")
+            file.write("_"*100+"\n")
             n += 1
             for data in all_data_list[30*x:30*X]:
-                print(f"{data[0] :<3d} {gap} {data[1] :<10d} {gap*2} {data[2] :<10.3f} {gap*3} {data[3] :<8d} {gap} {data[4] :10.4f} {gap} {data[5] :10.2f} {gap*3} {data[6] :<10.1f}")
+                file.write(f"{data[0] :<3d} {gap} {data[1] :<10d} {gap*2} {data[2] :<10.3f} {gap*3} {data[3] :<8d} {gap} {data[4] :10.4f} {gap} {data[5] :10.2f} {gap*3} {data[6] :<10.1f}"+"\n")
             x += 1
             X += 1
+            
+            
+def main():
 
-    def t_360(self):
-        #definierar variabler för for loop
+    powerplant_list = [ ]
+    latitud_list = [ ]
+    area = area_input()
+    prop_konst = prop_konst_input()
+
+    a = True
+    while a == True:
+        latitud = latitud_input()
+        kraftverk = Kraftverk(area, prop_konst, latitud)
+        #lista med objekt av krafverk för olika latituder
+        powerplant_list.append(kraftverk)
+        latitud_list.append(int(latitud))
+        
+        print("Add latitude(y/n):", end="") #frågar om fler latituder
+        add_latitud = str(input())
+        if add_latitud == "y":
+            a = True
+        if add_latitud == "n":
+            a = False
+
+    year_sum_list = [ ] 
+    all_pwrplant_all_data_list = [ ]
+    
+    for kraftverk in powerplant_list:
         t = 1 
         w_year_list = [ ]
         year_sum = 0
+        
         all_data_list = [ ]
     
         for i in range(360):
-            faktor = self.faktor()
-            v = self.v(t)
-            w = self.w(faktor, v)
+            faktor = kraftverk.faktor()
+            v = kraftverk.v(t)
+            w = kraftverk.w(faktor, v)
             #lista med enbart w för varje dag
             w_year_list.append(round(w, 3))
             year_sum += w
 
-            data = self.data(t, faktor, v, w)
+            data = kraftverk.data(t, faktor, v, w)
             all_data_list.append(data)
             t += 1
-                
-        std_dev = round(self.std_dev(year_sum, w_year_list), 3)
 
-        self.detail_data(all_data_list)
-            
-def main():
-    solkraftverk = Kraftverk(500, 5, 30)
-    solkraftverk_2 = Kraftverk(320, 7, 52)
+        year_sum_list.append(round(year_sum))
+        all_pwrplant_all_data_list.append(all_data_list)
+
+    #skapar sorterad dictionary med årsproduktion och lista med latitud och "index nr"
+    year_sum_dict = { }
+    i = 0
+    for w in year_sum_list:
+        year_sum_dict[latitud_list[i]] = [i, round(w/360, 1)]
+        i += 1
+    sort_year_sum_dict = dict(sorted(year_sum_dict.items()))
+
+    #presenterar resultat
+    i = 1
+    for key,value in sort_year_sum_dict.items():
+        print("Kraftverk Nr:", i,"Snitt effekt:","{[1]}".format(value),"W/dag", "Latitud:", "{}".format(key),"°")
+        i += 1
+        
+    print("For daily data, enter Nr of powerplant:",end="")
+    n = int(input())
+
+    # tar fram original index för att anropa rätt instans av klass
+    n = list(sort_year_sum_dict.values())[n-1][0]
     
-    solkraftverk_2.t_360()
+    
+    powerplant_list[n].detail_data("137.txt", all_pwrplant_all_data_list[n])
     
 main()
-
